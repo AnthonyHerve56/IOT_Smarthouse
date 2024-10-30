93ab1810a01b6b205a97ada4e93f671a3d57f8c7
AnthonyHerve56 <herve.anthony.pro@gmail.com>
2024-10-30 09:25:35 +0100

    Add steam_sensor in main and overlay

--- src/main.c
+++ src/main.c
@@ -11,15 +11,23 @@
 #include <zephyr/drivers/sensor.h>
 #include "../inc/lcd_screen_i2c.h"
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/adc.h>
 
 //Define
 #define LED_YELLOW_NODE DT_ALIAS(led_yellow)
 #define LCD_NODE DT_ALIAS(lcd)
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
 
 //Main code
 const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
 const struct i2c_dt_spec lcd_screen = I2C_DT_SPEC_GET(LCD_NODE);
 const struct device *const dht11 = DEVICE_DT_GET_ONE(aosong_dht); //Temperature
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
+
 int main(void) {
 	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
 	init_lcd(&lcd_screen);
